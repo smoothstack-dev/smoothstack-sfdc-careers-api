66f44f7e1e7ca86cb5d057f34bce6042d84700af
Oscar Noel <ocedanoescalante@email.arizona.edu>
2023-04-23 23:26:17 -0700

    Add missing topic

--- resources/sns/snsResources.ts
+++ resources/sns/snsResources.ts
@@ -1,22 +1,28 @@
 import { AWS } from '@serverless/typescript';
 
 export const snsResources: AWS['resources']['Resources'] = {
-    LinksGenerationTopic: {
-      Type: 'AWS::SNS::Topic',
-      Properties: {
-        TopicName: 'smoothstack-links-generation-sns-topic-v2',
-      },
+  LinksGenerationTopic: {
+    Type: 'AWS::SNS::Topic',
+    Properties: {
+      TopicName: 'smoothstack-links-generation-sns-topic-v2',
     },
-    AppointmentGenerationTopic: {
-      Type: 'AWS::SNS::Topic',
-      Properties: {
-        TopicName: 'smoothstack-appointment-generation-sns-topic-v2',
-      },
+  },
+  AppointmentGenerationTopic: {
+    Type: 'AWS::SNS::Topic',
+    Properties: {
+      TopicName: 'smoothstack-appointment-generation-sns-topic-v2',
     },
-    WebinarProcessingTopic: {
-      Type: 'AWS::SNS::Topic',
-      Properties: {
-        TopicName: 'smoothstack-webinar-processing-sns-topic-v2',
-      },
+  },
+  WebinarProcessingTopic: {
+    Type: 'AWS::SNS::Topic',
+    Properties: {
+      TopicName: 'smoothstack-webinar-processing-sns-topic-v2',
     },
+  },
+  DocumentGenerationTopic: {
+    Type: 'AWS::SNS::Topic',
+    Properties: {
+      TopicName: 'smoothstack-document-generation-sns-topic-v2',
+    },
+  },
 };
